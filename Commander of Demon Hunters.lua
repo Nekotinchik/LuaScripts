85eed547e8b6625ec81fb708860028d2fef0261f
Nekotinchik <mcace6666@mail.ru>
2020-05-05 04:13:07 +0300

    заменена структура и убран мусор

--- Commander of Demon Hunters.lua
+++ Commander of Demon Hunters.lua
@@ -1,87 +1,87 @@
-local NPC_EXAMPLE = 100091
+local Boss_Entry = 100091
 
-local NpcExample = {}
-
-local NpcExampleSay= {
+-- emote
+local emote= {
     [1] = "Come and die mortals...",
     [2] = "It's your power? hahaha",
 	[3] = "Oohh NOoOOOoo ...",
 };
 
-local NpcExampleSpells= {
-    [1] = 70986; --аура
-	[2] = 40827; --Sinful Beam
-	[3] = 33665; --Lightning Wrath
-	[4] = 53361; --frenzy
-	[5] = 65860; -- дубовая кожа
-	[6] = 58841 --frenzy
-};
+-- ability
 
-function NpcExample.OnEnterCombat(event, creature, target)        -- Start Phase 1 at 100% Health
-    creature:SendUnitYell(NpcExampleSay[1], 0)                     -- NPC Say
-    creature:RegisterEvent(NpcExample.OnPhase_1_Aura_Say, 1000, 0)
-	creature:RegisterEvent(NpcExample.OnPhase_1_Sinful_Beam_Say, 2500, 0)
-	creature:RegisterEvent(NpcExample.OnPhase_1_Lightning_Wrath_Say, 2000, 0)
-    creature:RegisterEvent(NpcExample.OnPhase_2, 3000, 0)            -- Check for next Phase
+local Shroud_Of_Sorrow = 70986; --аура
+local Sinful_Beam = 40827; --Sinful Beam
+local Lightning_Wrath = 33665; --Lightning Wrath
+local frenzy = 53361; --frenzy
+local Barkskin = 65860; -- дубовая кожа
+local frenzy_2 = 58841 --frenzy
+
+
+function OnEnterCombat(event, creature, target)        
+    creature:SendUnitYell(emote[1], 0)                   
+    creature:RegisterEvent(Shroud_Of_Sorrow, 1000, 0)
+	creature:RegisterEvent(Sinful_Beam, 2500, 0)
+	creature:RegisterEvent(Lightning_Wrath, 2000, 0)
+    creature:RegisterEvent(Phase_2, 3000, 0)            
 end
 
-function NpcExample.OnPhase_1_Aura_Say(event, delay, pCall, creature)
-    creature:CastSpell(creature:GetVictim(), NpcExampleSpells[1], 10000, 0) -- Npc Cast Spell
+function Shroud_Of_Sorrow(event, delay, pCall, creature)
+    creature:CastSpell(creature:GetVictim(), Shroud_Of_Sorrow, 10000, 0) 
 end
 
-function NpcExample.OnPhase_1_Sinful_Beam_Say(event, delay, pCall, creature)
-creature:CastSpell(creature:GetVictim(), NpcExampleSpells[2], 30000, 0)
+function Sinful_Beam(event, delay, pCall, creature)
+creature:CastSpell(creature:GetVictim(), Sinful_Beam, 30000, 0)
 end
 
-function NpcExample.OnPhase_1_Lightning_Wrath_Say(event, delay, pCall, creature)
-creature:CastSpell(creature:GetVictim(), NpcExampleSpells[3], 30000, 0)
+function Lightning_Wrath(event, delay, pCall, creature)
+creature:CastSpell(creature:GetVictim(), Lightning_Wrath, 30000, 0)
 end
 
-function NpcExample.OnPhase_2(event, delay, pCall, creature)
-    if (creature:GetHealthPct() <= 50) then                       -- Start Phase 2 at 75% Health
+function Phase_2(event, delay, pCall, creature)
+    if (creature:GetHealthPct() <= 50) then                       
         creature:RemoveEvents()
-        creature:SendUnitYell(NpcExampleSay[2], 0)                 -- Npc Say
-        creature:RegisterEvent(NpcExample.OnPhase_2_frenzy_Say, 2000, 1)
-		creature:RegisterEvent(NpcExample.OnPhase_2_koja_Say, 2000, 1)
-		creature:RegisterEvent(NpcExample.OnPhase_2_frenzy2_Say, 2000, 1)
-	    creature:RegisterEvent(NpcExample.OnPhase_1_Sinful_Beam_Say, 2500, 0)
-	    creature:RegisterEvent(NpcExample.OnPhase_1_Lightning_Wrath_Say, 3000, 0)
+        creature:SendUnitYell(emote[2], 0)                 
+        creature:RegisterEvent(frenzy, 2000, 1)
+		creature:RegisterEvent(Barkskin, 2000, 1)
+		creature:RegisterEvent(frenzy_2, 2000, 1)
+	    creature:RegisterEvent(Sinful_Beam, 2500, 0)
+	    creature:RegisterEvent(Lightning_Wrath, 3000, 0)
     end
 end
 
-function NpcExample.OnPhase_2_frenzy_Say(event, delay, pCall, creature)
-      creature:CastSpell(creature:GetVictim(), NpcExampleSpells[4]) 
-	  creature:RegisterEvent(NpcExample.OnPhase_2_frenzy_Say, 20000, 1)
+function frenzy(event, delay, pCall, creature)
+      creature:CastSpell(creature:GetVictim(), frenzy) 
+	  creature:RegisterEvent(frenzy, 20000, 1)
       end
 
 
-function NpcExample.OnPhase_2_koja_Say(event, delay, pCall, creature)
-      creature:CastSpell(creature:GetVictim(), NpcExampleSpells[5]) 
-	  creature:RegisterEvent(NpcExample.OnPhase_2_koja_Say, 20000, 1)-- Npc Cast Spell
+function Barkskin(event, delay, pCall, creature)
+      creature:CastSpell(creature:GetVictim(), Barkskin) 
+	  creature:RegisterEvent(Barkskin, 20000, 1)
       end
 
-function NpcExample.OnPhase_2_frenzy2_Say(event, delay, pCall, creature)
-      creature:CastSpell(creature:GetVictim(), NpcExampleSpells[6])  
-	  creature:RegisterEvent(NpcExample.OnPhase_2_frenzy2_Say, 7000, 1)-- Npc Cast Spell
+function frenzy_2(event, delay, pCall, creature)
+      creature:CastSpell(creature:GetVictim(), frenzy_2)  
+	  creature:RegisterEvent(frenzy_2, 7000, 1)
       end
 
 
 
-function NpcExample.OnLeaveCombat(event, creature)
+function OnLeaveCombat(event, creature)
 
     creature:RemoveEvents()
 end
 
-function NpcExample.OnTargetDied(event, creature, victim)
+function OnTargetDied(event, creature, victim)
 
 end
 
-function NpcExample.OnDied(event, creature, killer)
-    creature:SendUnitYell(NpcExampleSay[3], 0)                    -- Npc Say
+function OnDied(event, creature, killer)
+    creature:SendUnitYell(emote[3], 0)                   
     creature:RemoveEvents()
 end
 
-RegisterCreatureEvent(NPC_EXAMPLE, 1, NpcExample.OnEnterCombat)
-RegisterCreatureEvent(NPC_EXAMPLE, 2, NpcExample.OnLeaveCombat)
-RegisterCreatureEvent(NPC_EXAMPLE, 3, NpcExample.OnTargetDied)
-RegisterCreatureEvent(NPC_EXAMPLE, 4, NpcExample.OnDied)
+RegisterCreatureEvent(Boss_Entry, 1, OnEnterCombat)
+RegisterCreatureEvent(Boss_Entry, 2, OnLeaveCombat)
+RegisterCreatureEvent(Boss_Entry, 3, OnTargetDied)
+RegisterCreatureEvent(Boss_Entry, 4, OnDied)
